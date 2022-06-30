a0da800b5b4d51b0a0102653e0560e20a2adface
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-06-29 22:23:35 -0500

    Styles, Icons, JS, Font Awesome

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
 $(function () {
+    /*Tooltip*/
     $('[data-toggle="tooltip"]').tooltip()
   })
+    /*Tech Stack Show */
+  $('#techBtn').on('click', function(){
+    $('#tech').toggleClass('techShow');
+});
+    /*Client Login Hide on Expanded Navbar*/
+    $('#clientBtn').on('click', function(){
+        $('#clientLogin').toggleClass('clientHide');
+    });
+
+/*Back to Top Button */
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
